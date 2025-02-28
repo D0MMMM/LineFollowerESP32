d6e09c8f94846605415f9421826305964b8957ea
dom.dom <126216078+D0MMMM@users.noreply.github.com>
2025-03-01 07:32:11 +0800

    finished project

--- LineFollower.ino
+++ LineFollower.ino
@@ -1,67 +1,86 @@
-#define LEFT_SENSOR  32
-#define RIGHT_SENSOR 33
+// CMD - innovator tech company
 
-#define ENA  25
-#define ENB  26
+// Domenick B. Mahusay 
+// Albert Mark B. Alferez
+// Carlos A. Sode Jr.
 
-#define IN1  27
-#define IN2  14
-#define IN3  12
-#define IN4  13
 
-float Kp = 50.0;
-float Ki = 0.0;
-float Kd = 15.0;
 
-float error = 0, lastError = 0, integral = 0;
-int baseSpeed = 120;
-int maxSpeed = 200;
+// Define motor driver pins
+#define ENA 13
+#define IN1 12
+#define IN2 14
+#define ENB 27
+#define IN3 26
+#define IN4 25
+
+// Define IR sensor pins
+#define IR_LEFT 32
+#define IR_RIGHT 33
+
+// Define motor speed
+#define SPEED_FORWARD 140
+#define SPEED_TURN 80
+#define SPEED_STOP 0
 
 void setup() {
-    pinMode(LEFT_SENSOR, INPUT);
-    pinMode(RIGHT_SENSOR, INPUT);
-    
     pinMode(ENA, OUTPUT);
-    pinMode(ENB, OUTPUT);
     pinMode(IN1, OUTPUT);
     pinMode(IN2, OUTPUT);
+    pinMode(ENB, OUTPUT);
     pinMode(IN3, OUTPUT);
     pinMode(IN4, OUTPUT);
-    
-    Serial.begin(115200);
+    pinMode(IR_LEFT, INPUT);
+    pinMode(IR_RIGHT, INPUT);
 }
 
 void loop() {
-    int left = digitalRead(LEFT_SENSOR);
-    int right = digitalRead(RIGHT_SENSOR);
+    int leftSensor = digitalRead(IR_LEFT);
+    int rightSensor = digitalRead(IR_RIGHT);
 
-    if (left == 0 && right == 0) error = 0; 
-    else if (left == 1 && right == 0) error = -1;
-    else if (left == 0 && right == 1) error = 1;
-    else error = lastError;
-
-    float P = Kp * error;
-    integral += error;
-    float I = Ki * integral;
-    float D = Kd * (error - lastError);
-    float correction = P + I + D;
-    
-    int leftMotorSpeed = constrain(baseSpeed - correction, 0, maxSpeed);
-    int rightMotorSpeed = constrain(baseSpeed + correction, 0, maxSpeed);
-
-    moveMotors(leftMotorSpeed, rightMotorSpeed);
-    
-    lastError = error;
-
-    delay(10);
+    if (leftSensor == 1 && rightSensor == 0) {
+        turnRight();
+    } else if (leftSensor == 0 && rightSensor == 1) {
+        turnLeft();
+    } else if (leftSensor == 0 && rightSensor == 0) {
+        moveForward();
+    } else {
+        stopMotors();
+    }
 }
 
-void moveMotors(int leftSpeed, int rightSpeed) {
+void moveForward() {
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
-    
-    analogWrite(ENA, leftSpeed);
-    analogWrite(ENB, rightSpeed);
+    analogWrite(ENA, SPEED_FORWARD);
+    analogWrite(ENB, SPEED_FORWARD);
+}
+
+void turnRight() {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA, SPEED_TURN);
+    analogWrite(ENB, 140);
+}
+
+void turnLeft() {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENA, 140);
+    analogWrite(ENB, SPEED_TURN);
+}
+
+void stopMotors() {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA, SPEED_STOP);
+    analogWrite(ENB, SPEED_STOP);
 }
